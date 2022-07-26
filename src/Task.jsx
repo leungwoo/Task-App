a7da5f62106f1a8e6199a07c3e528844cf97a59c
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-07-26 17:09:28 -0400

    task app completed

---
+++ src/Task.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Task(props) {
+    return (
+        <div className='task'
+            style={{ backgroundColor: props.completed ? "green" : "white" }}>
+            <h1>{props.taskName}</h1>
+            <button onClick={() => props.completeTask(props.id)}>completed</button>
+            <button onClick={() => props.deleteTask(props.id)}>X</button>
+        </div>);
+};
+
